8f3a64d6a457c91c58e221dc2d73df44eff1af31
Fernando Castellanos <fernandocastellanos95@gmail.com>
2020-09-26 18:27:26 +0200

    Label + LBP plotting

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ import numpy as np
 parent_path = str(Path(dirname(abspath(__file__))).parent)
 preprocess = Preprocess(height=608, width=576)
 plot_results = PlotResults(height=608, width=576)
-path = parent_path + '/dataset/training/images/21_training.tif'
+img_path = parent_path + '/dataset/training/images/21_training.tif'
 mask_path = parent_path + '/dataset/training/mask/21_training_mask.tif'
-df = preprocess.apply_lbp(path, mask_path=mask_path)
+df = preprocess.apply_lbp(img_path, mask_path=mask_path)
 df.columns = ['1:1']
 label_path = parent_path + '/dataset/training/1st_manual/21_manual1.gif'
 df['target'] = preprocess.get_label(label_path)
-plot_results.plot_label(df)
+
+
+plot_results.plot_label_img(df)
--- plot_results/plot_results.py
+++ plot_results/plot_results.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 
 class PlotResults:
@@ -7,11 +8,24 @@ class PlotResults:
         self.height = height
         self.width = width
 
+    @staticmethod
+    def apply_red_mask(img, mask):
+        img = np.stack([img, img, img], axis=2)
+        mask = mask == 1
+        img[mask] = [255, 0, 0]  # red patch in upper left
+        return img
+
     def frame_to_img(self, df):
         return np.array(df).reshape(self.height, -1)
 
     def plot_label(self, df):
-        label = df.loc[:, 'target']
-        img = self.frame_to_img(label)
+        img = self.frame_to_img(df.loc[:, 'target'])
+        plt.figure()
+        plt.imshow(img, vmin=0, vmax=1, cmap='gray')
+
+    def plot_label_img(self, df):
+        img = self.frame_to_img(df.loc[:, '1:1'])
+        mask = self.frame_to_img(df.loc[:, 'target'])
+        img = self.apply_red_mask(img, mask)
         plt.figure()
         plt.imshow(img, vmin=0, vmax=1, cmap='gray')
