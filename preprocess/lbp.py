afbd447c2e0b53870b31158ff70e100f2b0da087
Fernando Castellanos <fernandocastellanos95@gmail.com>
2020-10-31 19:44:55 +0100

    LBP riu and riu2

--- preprocess/lbp.py
+++ preprocess/lbp.py
@@ -2,9 +2,15 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import itertools
 
 
-def lbp_pixel_calc(img, x, y, r=1):
+def roll_pattern(pattern, k):
+    rolled = np.roll(pattern, k)
+    return sum(rolled * (2 ** np.arange(len(rolled))[::-1]))
+
+
+def lbp_pixel_calc(img, x, y, r, method):
     center = img[x, y]
     matrix = img[x - r:x + r + 1, y - r:y + r + 1]
     if matrix.shape[0]*matrix.shape[1] != (r*2+1)**2:
@@ -19,11 +25,21 @@ def lbp_pixel_calc(img, x, y, r=1):
         matrix = img[x - r:x + r + 1, y - r:y + r + 1]
     arr = np.concatenate((matrix[0, 1:-1][::-1], matrix[:, 0], matrix[-1, 1:-1], matrix[0:, -1][::-1]))
     arr = np.where(arr < center, 1, 0)
-    return sum(arr*(2**np.arange(len(arr))[::-1]))
+    if method == 'riu':
+        n_arr = np.array(list(arr)*len(arr)).reshape(len(arr), -1)
+        return min(np.array([roll_pattern(v, i) for i, v in enumerate(n_arr)]))
+    elif method == 'riu2':
+        u = len(list(itertools.groupby(np.append(arr, arr[0]), lambda bit: bit == 0))) - 1
+        if u > 2:
+            return len(arr) + 1
+        else:
+            return sum(arr)
+    else:
+        return sum(arr * (2 ** np.arange(len(arr))[::-1]))
 
 
 def show_output(img_gray, img_lbp):
-    hist_lbp = cv2.calcHist([img_lbp], [0], None, [256], [0, 256])
+    hist_lbp = cv2.calcHist([img_lbp], [0], None, [10], [0, 10])
     output_list = [{
         "img": img_gray,
         "xlabel": "",
@@ -70,7 +86,7 @@ def show_output(img_gray, img_lbp):
             current_plot.set_ylabel(current_ylabel)
         elif current_type == "histogram":
             current_plot.plot(current_img, color="black")
-            current_plot.set_xlim([0, 260])
+            current_plot.set_xlim([0, 10])
             current_plot.set_title(current_title)
             current_plot.set_xlabel(current_xlabel)
             current_plot.set_ylabel(current_ylabel)
@@ -82,12 +98,12 @@ def show_output(img_gray, img_lbp):
     cv2.destroyAllWindows()
 
 
-def lbp(img_gray, plot=False):
+def lbp(img_gray, r=1, method='riu2', plot=False):
     height, width = img_gray.shape
     img_lbp = np.zeros((height, width), np.uint8)
     for i in range(0, height):
         for j in range(0, width):
-            img_lbp[i, j] = lbp_pixel_calc(img_gray, i, j)
+            img_lbp[i, j] = lbp_pixel_calc(img_gray, i, j, r, method)
     if plot:
         show_output(img_gray, img_lbp)
     return img_lbp
